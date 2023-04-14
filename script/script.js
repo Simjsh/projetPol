f9e29705a93ae8e014a9537ab5d668a9f5b128d3
Simjsh <sim.joseph@hotmail.com>
2023-04-14 22:46:28 +0200

    14/04/23

--- script/script.js
+++ script/script.js
@@ -3,7 +3,7 @@ function toutSelect(e, num) {
 };
 
 function affResult() {
-	$.post('resultats.php', $('form').serialize(), function(data) {  // Envoi les données du formulaire par la méthode POST au fichier secondaire et récupère une variable "data"
+	$.post('table.php', $('form').serialize(), function(data) {  // Envoi les données du formulaire par la méthode POST au fichier secondaire et récupère une variable "data"
   	$('#resultats').html(data).css('width', (data==="") ? '0%':'35%');   // Actualise les tables pour afficher la liste des spécialités et disparait si elle est vide
   	$('form').css('width', (data=="") ? '100%':'65%');     // La partie de gauche prend toute la longueur si la liste est vide
   })
